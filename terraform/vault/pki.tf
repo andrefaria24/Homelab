e59028b4872c9ee3dd251da8dc282ac816286dbd
Andre Faria <andre.faria.24@gmail.com>
2024-12-23 09:35:59 -0500

    formatting changes

--- terraform/vault/pki.tf
+++ terraform/vault/pki.tf
@@ -1,7 +1,7 @@
 # Enable PKI secrets engine
 resource "vault_mount" "pki" {
-  path = "pki"
-  type = "pki"
+  path        = "pki"
+  type        = "pki"
   description = "Root PKI mount"
 
   default_lease_ttl_seconds = 31536000  # 1 Year
@@ -46,66 +46,53 @@ resource "vault_pki_secret_backend_config_urls" "root_config_urls" {
 
 # Enable intermediate authority PKI secrets engine
 resource "vault_mount" "pki_int" {
-   path        = "pki_int"
-   type        = "pki"
-   description = "Intermediate PKI mount"
+  path        = "pki_int"
+  type        = "pki"
+  description = "Intermediate PKI mount"
 
-   default_lease_ttl_seconds = 31536000  # 1 Year
-   max_lease_ttl_seconds     = 315360000 # 10 Years
+  default_lease_ttl_seconds = 31536000  # 1 Year
+  max_lease_ttl_seconds     = 315360000 # 10 Years
 }
 
 # Generate an intermediate
 resource "vault_pki_secret_backend_intermediate_cert_request" "csr-request" {
-   backend     = vault_mount.pki_int.path
-   type        = "internal"
-   common_name = "${var.cn_root} Intermediate Authority"
+  backend     = vault_mount.pki_int.path
+  type        = "internal"
+  common_name = "${var.cn_root} Intermediate Authority"
 }
 
 # Sign with the root CA private key & save the generated certificate
 resource "vault_pki_secret_backend_root_sign_intermediate" "intermediate" {
-   backend     = vault_mount.pki.path
-   common_name = var.cn_intermediate
-   csr         = vault_pki_secret_backend_intermediate_cert_request.csr-request.csr
-   format      = "pem_bundle"
-   ttl         = 315360000 # 10 Years
-   issuer_ref  = vault_pki_secret_backend_root_cert.pki_root.issuer_id
+  backend     = vault_mount.pki.path
+  common_name = var.cn_intermediate
+  csr         = vault_pki_secret_backend_intermediate_cert_request.csr-request.csr
+  format      = "pem_bundle"
+  ttl         = 315360000 # 10 Years
+  issuer_ref  = vault_pki_secret_backend_root_cert.pki_root.issuer_id
 }
 
 # Import cert back to Vault
 resource "vault_pki_secret_backend_intermediate_set_signed" "intermediate" {
-   backend     = vault_mount.pki_int.path
-   certificate = vault_pki_secret_backend_root_sign_intermediate.intermediate.certificate
+  backend     = vault_mount.pki_int.path
+  certificate = vault_pki_secret_backend_root_sign_intermediate.intermediate.certificate
 }
 
 # Manually imported
 resource "vault_pki_secret_backend_issuer" "intermediate" {
-  backend = vault_mount.pki_int.path
+  backend    = vault_mount.pki_int.path
   issuer_ref = "90ffcf38-7eee-a9d5-0961-ba4392ab3f18"
 }
 
 # Create intermediate CA role
 resource "vault_pki_secret_backend_role" "intermediate_role" {
-   backend          = vault_mount.pki_int.path
-   issuer_ref       = vault_pki_secret_backend_issuer.intermediate.issuer_ref
-   name             = var.cn_intermediate_role_name
-   ttl              = 31536000 # 10 Years
-   max_ttl          = 31536000 # 10 Years
-   allow_ip_sans    = true
-   key_type         = "rsa"
-   key_bits         = 4096
-   allowed_domains  = var.allowed_domains
-   allow_subdomains = true
-}
-
-###                            ###
-###   GENERATED CERTIFICATES   ###
-###                            ###
-
-resource "vault_pki_secret_backend_cert" "test" {
-  issuer_ref  = vault_pki_secret_backend_issuer.intermediate.issuer_ref
-  backend     = vault_pki_secret_backend_role.intermediate_role.backend
-  name        = vault_pki_secret_backend_role.intermediate_role.name
-  common_name = "test.${var.cn_intermediate}"
-  ttl         = 31536000 # 1 Year
-  revoke     = true
+  backend          = vault_mount.pki_int.path
+  issuer_ref       = vault_pki_secret_backend_issuer.intermediate.issuer_ref
+  name             = var.cn_intermediate_role_name
+  ttl              = 31536000 # 10 Years
+  max_ttl          = 31536000 # 10 Years
+  allow_ip_sans    = true
+  key_type         = "rsa"
+  key_bits         = 4096
+  allowed_domains  = var.allowed_domains
+  allow_subdomains = true
 }

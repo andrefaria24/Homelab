dc87ecbbfaaf7f67b3004e247e13d49dc8bd5ea8
Andre Faria <andre.faria.24@gmail.com>
2025-06-06 17:10:03 -0400

    removed certs.tf from ignorefile

---
+++ terraform/vault/certs.tf
@@ -0,0 +1,183 @@
+locals {
+  cert_ttl = 63072000 # 2 Years
+}
+
+# Vault
+resource "vault_pki_secret_backend_cert" "vault" {
+  issuer_ref  = vault_pki_secret_backend_issuer.intermediate.issuer_ref
+  backend     = vault_pki_secret_backend_role.intermediate_role.backend
+  name        = vault_pki_secret_backend_role.intermediate_role.name
+  common_name = "vault.${var.cn_intermediate}"
+  ttl         = 31536000 # 10 Years
+  revoke      = true
+}
+
+resource "local_file" "vault_pvt_key" {
+  content  = vault_pki_secret_backend_cert.vault.private_key
+  filename = "./certs/${vault_pki_secret_backend_cert.vault.common_name}.key"
+}
+
+resource "local_file" "vault_pub_key" {
+  content  = vault_pki_secret_backend_cert.vault.certificate
+  filename = "./certs/${vault_pki_secret_backend_cert.vault.common_name}.crt"
+}
+
+# Pi-hole
+resource "vault_pki_secret_backend_cert" "pihole" {
+  issuer_ref  = vault_pki_secret_backend_issuer.intermediate.issuer_ref
+  backend     = vault_pki_secret_backend_role.intermediate_role.backend
+  name        = vault_pki_secret_backend_role.intermediate_role.name
+  common_name = "faria-pi.${var.cn_intermediate}"
+  ttl         = local.cert_ttl
+  revoke      = true
+}
+
+resource "local_file" "pihole_pvt_key" {
+  content  = vault_pki_secret_backend_cert.pihole.private_key
+  filename = "./certs/${vault_pki_secret_backend_cert.pihole.common_name}.key"
+}
+
+resource "local_file" "pihole_pub_key" {
+  content  = vault_pki_secret_backend_cert.pihole.certificate
+  filename = "./certs/${vault_pki_secret_backend_cert.pihole.common_name}.crt"
+}
+
+# Portainer
+resource "vault_pki_secret_backend_cert" "portainer" {
+  issuer_ref  = vault_pki_secret_backend_issuer.intermediate.issuer_ref
+  backend     = vault_pki_secret_backend_role.intermediate_role.backend
+  name        = vault_pki_secret_backend_role.intermediate_role.name
+  common_name = "portainer.${var.cn_intermediate}"
+  ttl         = local.cert_ttl
+  revoke      = true
+}
+
+resource "local_file" "portainer_pvt_key" {
+  content  = vault_pki_secret_backend_cert.portainer.private_key
+  filename = "./certs/${vault_pki_secret_backend_cert.portainer.common_name}.key"
+}
+
+resource "local_file" "portainer_pub_key" {
+  content  = vault_pki_secret_backend_cert.portainer.certificate
+  filename = "./certs/${vault_pki_secret_backend_cert.portainer.common_name}.crt"
+}
+
+# UptimeKuma
+resource "vault_pki_secret_backend_cert" "uptimekuma" {
+  issuer_ref  = vault_pki_secret_backend_issuer.intermediate.issuer_ref
+  backend     = vault_pki_secret_backend_role.intermediate_role.backend
+  name        = vault_pki_secret_backend_role.intermediate_role.name
+  common_name = "monitoring.${var.cn_intermediate}"
+  ttl         = local.cert_ttl
+  revoke      = true
+}
+
+resource "local_file" "uptimekuma_pvt_key" {
+  content  = vault_pki_secret_backend_cert.uptimekuma.private_key
+  filename = "./certs/${vault_pki_secret_backend_cert.uptimekuma.common_name}.key"
+}
+
+resource "local_file" "uptimekuma_pub_key" {
+  content  = vault_pki_secret_backend_cert.uptimekuma.certificate
+  filename = "./certs/${vault_pki_secret_backend_cert.uptimekuma.common_name}.crt"
+}
+
+# Router (RT-AX86U Pro)
+resource "vault_pki_secret_backend_cert" "router" {
+  issuer_ref  = vault_pki_secret_backend_issuer.intermediate.issuer_ref
+  backend     = vault_pki_secret_backend_role.intermediate_role.backend
+  name        = vault_pki_secret_backend_role.intermediate_role.name
+  common_name = "faria-router.${var.cn_intermediate}"
+  ttl         = local.cert_ttl
+  revoke      = true
+}
+
+resource "local_file" "router_pvt_key" {
+  content  = vault_pki_secret_backend_cert.router.private_key
+  filename = "./certs/${vault_pki_secret_backend_cert.router.common_name}.key"
+}
+
+resource "local_file" "router_pub_key" {
+  content  = vault_pki_secret_backend_cert.router.certificate
+  filename = "./certs/${vault_pki_secret_backend_cert.router.common_name}.crt"
+}
+
+# Plex
+resource "vault_pki_secret_backend_cert" "plex" {
+  issuer_ref  = vault_pki_secret_backend_issuer.intermediate.issuer_ref
+  backend     = vault_pki_secret_backend_role.intermediate_role.backend
+  name        = vault_pki_secret_backend_role.intermediate_role.name
+  common_name = "faria-plex.${var.cn_intermediate}"
+  ttl         = local.cert_ttl
+  revoke      = true
+}
+
+resource "local_file" "plex_pvt_key" {
+  content  = vault_pki_secret_backend_cert.plex.private_key
+  filename = "./certs/${vault_pki_secret_backend_cert.plex.common_name}.key"
+}
+
+resource "local_file" "plex_pub_key" {
+  content  = vault_pki_secret_backend_cert.plex.certificate
+  filename = "./certs/${vault_pki_secret_backend_cert.plex.common_name}.crt"
+}
+
+# Proxmox
+resource "vault_pki_secret_backend_cert" "proxmox" {
+  issuer_ref  = vault_pki_secret_backend_issuer.intermediate.issuer_ref
+  backend     = vault_pki_secret_backend_role.intermediate_role.backend
+  name        = vault_pki_secret_backend_role.intermediate_role.name
+  common_name = "faria-proxmox.${var.cn_intermediate}"
+  ttl         = local.cert_ttl
+  revoke      = true
+}
+
+resource "local_file" "proxmox_pvt_key" {
+  content  = vault_pki_secret_backend_cert.proxmox.private_key
+  filename = "./certs/${vault_pki_secret_backend_cert.proxmox.common_name}.key"
+}
+
+resource "local_file" "proxmox_pub_key" {
+  content  = vault_pki_secret_backend_cert.proxmox.certificate
+  filename = "./certs/${vault_pki_secret_backend_cert.proxmox.common_name}.crt"
+}
+
+# Synology NAS
+resource "vault_pki_secret_backend_cert" "nas" {
+  issuer_ref  = vault_pki_secret_backend_issuer.intermediate.issuer_ref
+  backend     = vault_pki_secret_backend_role.intermediate_role.backend
+  name        = vault_pki_secret_backend_role.intermediate_role.name
+  common_name = "faria-nas.${var.cn_intermediate}"
+  ttl         = local.cert_ttl
+  revoke      = true
+}
+
+resource "local_file" "nas" {
+  content  = vault_pki_secret_backend_cert.nas.private_key
+  filename = "./certs/${vault_pki_secret_backend_cert.nas.common_name}.key"
+}
+
+resource "local_file" "nas_pub_key" {
+  content  = vault_pki_secret_backend_cert.nas.certificate
+  filename = "./certs/${vault_pki_secret_backend_cert.nas.common_name}.crt"
+}
+
+# Terraform
+resource "vault_pki_secret_backend_cert" "terraform" {
+  issuer_ref  = vault_pki_secret_backend_issuer.intermediate.issuer_ref
+  backend     = vault_pki_secret_backend_role.intermediate_role.backend
+  name        = vault_pki_secret_backend_role.intermediate_role.name
+  common_name = "faria-terraform.${var.cn_intermediate}"
+  ttl         = local.cert_ttl
+  revoke      = true
+}
+
+resource "local_file" "terraform" {
+  content  = vault_pki_secret_backend_cert.terraform.private_key
+  filename = "./certs/${vault_pki_secret_backend_cert.terraform.common_name}.key"
+}
+
+resource "local_file" "terraform_pub_key" {
+  content  = vault_pki_secret_backend_cert.terraform.certificate
+  filename = "./certs/${vault_pki_secret_backend_cert.terraform.common_name}.crt"
+}

4a8fbc00065245627c3a30d74bd887bb1b2f6686
Andre Faria <andre.faria.24@gmail.com>
2025-06-10 19:36:06 -0400

    added vault & proxmox providers

--- terraform/tfe/main.tf
+++ terraform/tfe/main.tf
@@ -7,3 +7,19 @@ resource "tfe_project" "default" {
   organization = tfe_organization.default.name
   name         = "Default Project"
 }
+
+resource "tfe_registry_provider" "vault" {
+  organization = tfe_organization.default.name
+
+  registry_name = "public"
+  namespace     = "hashicorp"
+  name          = "vault"
+}
+
+resource "tfe_registry_provider" "proxmox" {
+  organization = tfe_organization.default.name
+
+  registry_name = "public"
+  namespace     = "telmate"
+  name          = "proxmox"
+}

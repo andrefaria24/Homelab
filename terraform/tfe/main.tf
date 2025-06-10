3cecd59632263ca864e492f5d9c033a74069f875
Andre Faria <andre.faria.24@gmail.com>
2025-06-10 14:35:35 -0400

    Added default project

--- terraform/tfe/main.tf
+++ terraform/tfe/main.tf
@@ -2,3 +2,8 @@ resource "tfe_organization" "default" {
   name  = var.tfe_organization
   email = var.tfe_email
 }
+
+resource "tfe_project" "default" {
+  organization = tfe_organization.default.name
+  name         = "Default Project"
+}

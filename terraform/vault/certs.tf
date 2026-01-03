2d09ad1c04b9c31d6e30530cccf68040dbea863e
Andre Faria <andre.faria.24@gmail.com>
2026-01-03 09:40:09 -0500

    added plex container logic

--- terraform/vault/certs.tf
+++ terraform/vault/certs.tf
@@ -108,7 +108,7 @@ resource "vault_pki_secret_backend_cert" "plex" {
   issuer_ref  = vault_pki_secret_backend_issuer.intermediate.issuer_ref
   backend     = vault_pki_secret_backend_role.intermediate_role.backend
   name        = vault_pki_secret_backend_role.intermediate_role.name
-  common_name = "plex.${var.cn_intermediate}"
+  common_name = "faria-plex.${var.cn_intermediate}"
   ttl         = local.cert_ttl
   revoke      = true
 }
@@ -182,22 +182,22 @@ resource "local_file" "nas_pub_key" {
   filename = "${local.cert_location}${vault_pki_secret_backend_cert.nas.common_name}.crt"
 }
 
-# n8n
-resource "vault_pki_secret_backend_cert" "n8n" {
+# Pulse
+resource "vault_pki_secret_backend_cert" "pulse" {
   issuer_ref  = vault_pki_secret_backend_issuer.intermediate.issuer_ref
   backend     = vault_pki_secret_backend_role.intermediate_role.backend
   name        = vault_pki_secret_backend_role.intermediate_role.name
-  common_name = "n8n.${var.cn_intermediate}"
+  common_name = "pulse.${var.cn_intermediate}"
   ttl         = local.cert_ttl
   revoke      = true
 }
 
-resource "local_file" "n8n" {
-  content  = vault_pki_secret_backend_cert.n8n.private_key
-  filename = "${local.cert_location}${vault_pki_secret_backend_cert.n8n.common_name}.key"
+resource "local_file" "pulse" {
+  content  = vault_pki_secret_backend_cert.pulse.private_key
+  filename = "${local.cert_location}${vault_pki_secret_backend_cert.pulse.common_name}.key"
 }
 
-resource "local_file" "n8n_pub_key" {
-  content  = vault_pki_secret_backend_cert.n8n.certificate
-  filename = "${local.cert_location}${vault_pki_secret_backend_cert.n8n.common_name}.crt"
+resource "local_file" "pulse_pub_key" {
+  content  = vault_pki_secret_backend_cert.pulse.certificate
+  filename = "${local.cert_location}${vault_pki_secret_backend_cert.pulse.common_name}.crt"
 }
